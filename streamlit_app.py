7788dea06455ff2cc826f78511a4da96a7feb03b
giasnowflake <119959902+giasnowflake@users.noreply.github.com>
2022-12-08 09:53:01 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.header('Breakfast Menu 🍌')
 streamlit.text('🥚Eggs and Beans')
 streamlit.text('🍞Toast Water')
 import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
